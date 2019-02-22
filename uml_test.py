23985e0a19b08f76e0c681cb974d78c432b7132c
LeeKLTW <LeeKLTW@gmail.com>
2019-02-22 12:57:41 +0800

    science, & uml

---
+++ uml_test.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+class A:
+    def __init__(self):
+        pass
+
+class B:
+    def __init__(self):
+        pass
+
+class C(A):
+    def __init__(self):
+        pass
+
+class D(B,C):
+    def __init(self):
+        pass
+
+d = D()
+
+def test():
+    pass
+
